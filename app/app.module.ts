cefb777e4d7717676e4b21f711fc3783593b1c48
David Casanellas <david.casanellas@weareblueorange.com>
2017-11-22 11:43:18 +0100

    feat: auth-form barrel

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AuthFormModule } from './auth-form/auth-form.module';
-import { AuthRememberComponent } from './auth-form/auth-remember.component';
+import {
+  AuthFormModule,
+  AuthRememberComponent
+} from './auth-form/';
 
 import { AppComponent } from './app.component';
 
---
+++ app/auth-form/index.ts
@@ -0,0 +1,2 @@
+export * from './auth-form.module';
+export * from './auth-remember.component';
