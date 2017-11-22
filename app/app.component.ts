5c458e6e76e8ee6f43c710de4c1a29ca10fcc010
David Casanellas <david.casanellas@weareblueorange.com>
2017-11-22 12:10:26 +0100

    chore: @ContentChildren and QueryLists

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,8 +17,10 @@ import { User } from './auth-form/auth-form.interface';
         (submitted)="loginUser($event)">
         <h3>Login</h3>
         <auth-remember (checked)="rememberUser($event)"></auth-remember>
+        <auth-remember (checked)="rememberUser($event)"></auth-remember>
+        <auth-remember (checked)="rememberUser($event)"></auth-remember>
         <button type="submit">
-        Login
+          Login
         </button>
       </auth-form>
     </div>
