8de48e2c323394f2097c73945d1c55f9d020051a
David Casanellas <david.casanellas@weareblueorange.com>
2017-11-30 18:13:14 +0100

    feat: reset to mail app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
-interface File {
-  name: string,
-  size: any,
-  type: string
-}
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['app.component.scss'],
   template: `
-    <div>
-      <stock-inventory></stock-inventory>
+    <div class="app">
+      <header>
+        <img src="/img/logo.svg">
+      </header>
+      <div class="app__content">
+        <nav>
+          <a
+            routerLink="folder/inbox"
+            routerLinkActive="active">
+            Inbox
+          </a>
+          <a
+            routerLink="folder/trash"
+            routerLinkActive="active">
+            Trash
+          </a>
+        </nav>
+        <mail-app></mail-app>
+      </div>
     </div>
   `
 })
 export class AppComponent implements OnInit {
-
-  ngOnInit() {
-
-  }
+  constructor(private router: Router) {}
+  ngOnInit() {}
 }
